5b2e6143d10ad473ec5f614df48b52ed57f291e4
kabamaplaying <kbmplaying@hotmail.com>
2020-02-26 15:55:03 -0500

    1 file modified modificacion actions

--- src/app/post.action.ts
+++ src/app/post.action.ts
@@ -1,6 +1,6 @@
 import { createAction, props } from '@ngrx/store';
 import { Post } from './post';
-export const GET_ALL_POST = createAction('[ALL] Post');
+export const getAllPost = createAction('[ALL] Post');
 export const GET_ALL_POST_SUCCES = createAction('[ALL] Post Success',
   props<{ payload: Post[] }>()
 );
